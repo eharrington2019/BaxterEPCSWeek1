5d2f8c8182957ada37f0a0ed39f7d7a5bc739c6b
eharrington2019 <32171170+eharrington2019@users.noreply.github.com>
2017-10-02 10:37:02 -0400

    made the code work

--- studentInfo.py
+++ studentInfo.py
@@ -1,35 +1,63 @@
+import random
+
+lastNames=["Smith", "Johnson", "Williams", "Brown", "Jones", "Miller", "Davis", "Garcia", "Rodriguez", "Wilson", "Martinez", "Anderson", "Taylor", "Thomas", "Hernandez", "Moore", "Martin", "Jackson", "Thompson", "White"]
+
 def main():
   students = [
-    Student("Larsson", 37),
-    Student("BonJovi", 55),
+    Student("Larsson", "Hal", 47, 250, 6),
+    Student("BonJovi", "Jon", 55, 135, 5),
+    Student("BonJovi", "Jon", 55, 135, 5),
+    Student("BonJovi", "Jon", 55, 135, 5),
+    Student("BonJovi", "Jon", 55, 135, 5),
+    Student("BonJovi", "Jon", 55, 135, 5),
+    Student("BonJovi", "Jon", 55, 135, 5),
+    Student("BonJovi", "Jon", 55, 135, 5),
+    Student("BonJovi", "Jon", 55, 135, 5),
+    Student("BonJovi", "Jon", 55, 135, 5),
+    Student("BonJovi", "Jon", 55, 135, 5),
   ]
 
   printHeader()
-  selection = getUserSelection()
+  selection = int(getUserSelection())
+  if selection == 0:
+    printStudentsByAge(students)
+  elif selection == 1:
+    printStudentsBylastName(students)
+  elif selection == 2:
+    printStudentsByfirstName(students)
+  elif selection == 3:
+    printSumAge
+  else:
+    print ("SELECTION NOT RECOGNIZED")
 
 class Student:
-  def __init__(self, lastName, age):
-    self.lastName = lastName
-    self.age = age
-    self.firstName = "NONE SPECIFIED"
+  def __init__(self, lastName, firstname, age, weight, height):
+    self.assignRandomlastName()
+    self.assignRandomfirstName()
+    self.assignRandomAge()
+    self.assignRandomWeight()
+    self.assignRandomHeight()
 
-  def assignRandomName(self):
-    pass
+  def assignRandomlastName(self):
+    self.lastName = random.choice (lastNames)
+
+  def assignRandomfirstName(self):
+    self.firstName = random.choice (firstNames)
 
   def assignRandomAge(self):
-    pass
+    self.age = random.randint(0,100)
 
-  def assignRandomWeight(self, isMetric):
-    pass
+  def assignRandomWeight(self):
+    self.weight = random.randint(100,300)
 
-  def assignRandomHeight(self, isMetric):
-    pass
+  def assignRandomHeight(self):
+    self.height = random.randint(5,7)
 
 inputQuestions = [
   "For STUDENTS BY AGE, type 0",
   "For STUDENTS BY LAST NAME, type 1",
-  "For STUDENTS BY FIRST NAME, type 3",
-  "For SUM of STUDENT AGES type 4",
+  "For STUDENTS BY FIRST NAME, type 2",
+  "For SUM of STUDENT AGES type 3",
   "For AVERAGE of STUDENT AGES type 5",
 ]
 
@@ -37,29 +65,43 @@ def getUserSelection():
   print (inputQuestions[0])
   print (inputQuestions[1])
   print (inputQuestions[2])
+  print (inputQuestions[3])
+  print (inputQuestions[4])
   return input("Type selection and press enter:")
 
-
 def printHeader():
-    print("HEADER TEXT HERE")
+    print("STUDENT SORTING")
 
-def printStudentsByAge():
+def printStudent(student):
+    print (student.lastName + ", " + student.firstName + ", " + str(student.age) + ", " + str(student.weight) + ", " + str(student.height))
+
+
+def printStudentsByAge(students):
   print ("----Students By Age-----")
+  sortStudents = sorted(students, key=lambda student: student.age)
+  for student in sortStudents:
+      printStudent(student)
 
-def printStudentsLName():
+def printStudentsBylastName(students):
   print ("----Students By -----")
+  sortStudents = sorted(students, key=lambda student: student.lastName)
+  for student in sortStudents:
+    printStudent(student)
 
-def printStudentsFName():
+def printStudentsByfirstName(students):
   print ("----Students By -----")
+  sortStudents = sorted(students, key=lambda student: student.firstName)
+  for student in sortStudents:
+    printStudent(student)
 
-def printSumAge():
+def printSumAge(students):
   print ("Answer:")
 
-def printAvgAge():
+def printAvgAge(students):
   print ("Answer:")
 
 def ageRange(studentA, studentB):
-  pass
+  return math.abs(studentA.age - studentB.age)
 
 
 main()
